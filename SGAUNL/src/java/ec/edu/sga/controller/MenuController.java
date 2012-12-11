6d3b656e69f6331794c2640f9c05f486152b7e75
luchitoflores84 <luchitoflores84@8127c0de-26e7-2408-c742-06ec58287e58>
2012-12-11 17:47:07 +0000

    Cambio de sesion de menu controller a conversación

--- SGAUNL/src/java/ec/edu/sga/controller/MenuController.java
+++ SGAUNL/src/java/ec/edu/sga/controller/MenuController.java
@@ -1,35 +1,43 @@
 package ec.edu.sga.controller;
 
+import ec.edu.sga.controller.util.JsfUtil;
 import ec.edu.sga.controller.util.SessionUtil;
 import ec.edu.sga.facade.MenuFacade;
-import java.util.Date;
-import java.util.List;
-import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
-import javax.faces.model.SelectItem;
 import ec.edu.sga.modelo.usuarios.Menu;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
 /**
  *
  * @author juanmanuelmarchese
  */
 @ManagedBean
-@SessionScoped
+@ConversationScoped
 public class MenuController implements Serializable{
 
     @EJB
     private MenuFacade dao;
+    @EJB
+    private MenuFacade ejbFacade;
     private Menu selected;
+    private Menu current;
+     @Inject
+    Conversation conversation;
+     private Long menuId;
 
     // ---------------------- Constructor de la Clase ----------------------
 
     public MenuController() {
-        
+        current = new Menu();
         selected = new Menu();
     }
 
@@ -103,6 +111,132 @@ public class MenuController implements Serializable{
 
     }
 
+    public Menu getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Menu current) {
+        System.out.println("Ingreso a fijar curso: " + current);
+        this.beginConversation();
+        this.current = current;
+    }
+
+    
+    
+     public Long getMenuId() {
+        if (current != null) {
+            menuId = current.getId();
+            return menuId;
+        }
+
+        return null;
+    }
+
+    public void setMenuId(Long menuId) {
+
+        System.out.println("========> Ingreso a fijar el id de un Menu: " + menuId);
+        this.beginConversation();
+        if (menuId != null && menuId.longValue() > 0) {
+
+           this.menuId = this.current.getId();
+
+            
+            System.out.println("========> INGRESO a Editar un Menu: " + current.getNombre());
+        } else {
+            System.out.println("========> INGRESO a Crear un Menu: ");
+            this.current = new Menu();
+        }
+
+
+    }
   
+    
+    
+    
+    
+    
+    
+    
+    //________________MÉTODOS PARA INICIALIZAR Y FINALIZAR LA CONVERSACIÓN_________//
+    public void beginConversation() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+            System.out.println("========> INICIANDO CONVERSACION: ");
+        }
+    }
+
+    public void endConversation() {
+        if (!conversation.isTransient()) {
+            conversation.end();
+            System.out.println("========> FINALIZANDO CONVERSACION: ");
+        }
+    }
+
+    public String cancelEdit() {
+        System.out.println("me acaban de llamar: canceledit()");
+        this.endConversation();
+        return "/index?faces-redirect=true";
+    }
+
+    //_______________________PERSISTIR OBJETOS________________________________//
+    public String createInstance() {
+
+        System.out.println("========> INGRESO a Crear una instancia de Menu: " + current.getNombre());
+        this.current = new Menu();
+        return "/index?faces-redirect=true";
+       
+    }
+
+    public String persist() {
+
+        System.out.println("========> INGRESO a Grabar nuevo Menu: " + current.getNombre());
+        current.setCreated(new Date());
+        current.setUpdated(new Date());
+        ejbFacade.create(current);
+        this.endConversation();
+
+        String summary = ResourceBundle.getBundle("/Bundle").getString("CursoCreated");
+        JsfUtil.addSuccessMessage(summary);
+
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+
+        return "/index?faces-redirect=true";
+
+
+    }
+
+    public String update() {
+
+        System.out.println("========> INGRESO a Actualizar al Curso: " + current.getNombre());
+        current.setUpdated(new Date());
+        ejbFacade.edit(current);
+        System.out.println("ya modifique");
+        this.endConversation();
+
+        String summary = ResourceBundle.getBundle("/Bundle").getString("EstudianteUpdated");
+        FacesContext.getCurrentInstance().addMessage("successInfo", new FacesMessage(FacesMessage.SEVERITY_INFO, summary, summary));
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+
+        return "/index?faces-redirect=true";
+
+    }
+
+    public String delete() {
+        System.out.println("========> INGRESO a Eliminar Curso: " + current.getNombre());
+        ejbFacade.remove(current);
+
+        //cambia este método por uno implementado con búsqueda por criteria
+      //  this.findAll();
+
+        this.endConversation();
+
+        String summary = "Curso Eliminado Correctamente!";
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null));
+
+
+        return "/curso/List?faces-redirect=true";
+
+    }
+    
 
 }
